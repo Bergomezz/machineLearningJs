cc72ce447e5780f14df8f906d4e1ca20cc589d20
root <root@DESKTOP-94VVHVT>
2023-09-08 14:47:28 -0300

    initial set, scored and analysis function

---
+++ plinko/score.js
@@ -0,0 +1,36 @@
+//import _ from 'loadash';
+
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  console.log('your point will probably fall into', bucket);
+}
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+}
