20978362b758f2cd4e071dfc25287561aedd5a50
root <root@DESKTOP-94VVHVT>
2023-09-11 15:24:38 -0300

    feature and refactory - knn function and runAnalysis

--- plinko/score.js
+++ plinko/score.js
@@ -1,7 +1,6 @@
 //import _ from 'loadash';
 
 const outputs = [];
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -9,8 +8,24 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+  let numberCorrect = 0;
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+
+    if (bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
+  }
+
+  console.log('Accuracy: ', numberCorrect / testSetSize);
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -20,12 +35,10 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  console.log('your point will probably fall into', bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
 }
 
 function splitDataset(data, testCount) {
@@ -33,4 +46,6 @@ function splitDataset(data, testCount) {
 
   const testSet = _.slice(shuffled, 0, testCount);
   const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 }
